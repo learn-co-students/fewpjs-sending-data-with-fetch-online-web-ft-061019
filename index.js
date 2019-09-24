08e0b5d4e5f609e3ec07d4b9b54aab07209fee67
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-23 18:03:55 -0600

    Done.

--- index.js
+++ index.js
@@ -6,55 +6,32 @@
 //     2) handles the POST request response, retrieves the new id value and appends it to the DOM
 //     3) handles a failed POST request using catch, appends the error message to the DOM
 
-// function submitData(name, email) {
-//    const createUserUrl = "http://localhost:3000/users"
-//    const fetchOptions = {
-//       method: "POST",
-//       headers: { 
-//          "Content-Type": "application/json",
-//          "Accept": "application/json"
-//       },
-//       body: JSON.stringify({
-//          name, 
-//          email
-//       })
-//    }
-//    fetch(createUserUrl, fetchOptions)
-//       .then(function(response) {
-//          return response.clone().json();
-//       })
-//       .then(function(json) {
-//          console.log(json)
-//          const newId = document.createElement("p")
-//          newId.textContent = json["id"]
-//          document.body.appendChild(newId)
-//       })
-//       .catch(function(error) {
-//          alert("Bad things! happened...");
-//          const newErr = document.createElement("p")
-//          newErr.textContent = error.message
-//          document.body.appendChild(newErr)
-//       });
-// }
-function submitData( name, email ) {
-   return fetch( 'http://localhost:3000/users', {
-       method: "POST",
-       headers: {
+function submitData(name, email) {
+   const createUserUrl = "http://localhost:3000/users"
+   const fetchOptions = {
+      method: "POST",
+      headers: { 
          "Content-Type": "application/json",
          "Accept": "application/json"
-       },
-       body: JSON.stringify( {
-         name,
+      },
+      body: JSON.stringify({
+         name, 
          email
-       } )
-     } )
-     .then( function ( response ) {
-       return response.json()
-     } )
-     .then( function ( object ) {
-       document.body.innerHTML = object[ "id" ]
-     } )
-     .catch( function ( error ) {
-       document.body.innerHTML = error.message
-     } )
- }
+      })
+   }
+   return fetch(createUserUrl, fetchOptions)
+      .then(function(response) {
+         return response.json();
+      })
+      .then(function(json) {
+         console.log(json)
+         const newId = document.createElement("p")
+         newId.textContent = json["id"]
+         document.body.appendChild(newId)
+      })
+      .catch(function(error) {
+         const newErr = document.createElement("p")
+         newErr.textContent = error.message
+         document.body.appendChild(newErr)
+      });
+}
