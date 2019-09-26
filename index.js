8180880b5c6788b3e3b66542affb2a01ffcda737
Mic Fox <michael.c.fox.ii@gmail.com>
2019-09-26 14:22:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+
+        .then(function(response){
+            // console.log(response)
+            return response.json();
+        })
+        .then(function(json){
+            console.log(json)
+            const newId = document.createElement("p")
+            newId.innerHTML = json["id"]
+            document.body.appendChild(newId)
+        })
+        .catch(function(error) {
+            const elementError = document.createElement("p")
+            elementError.innerHTML = error.message
+            document.body.appendChild(elementError)
+    })
+}
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     submitData(name, email)
+// })
