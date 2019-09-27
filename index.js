e576e5b2a2d355525a4530cde825f243977255fa
github username <github email address>
2019-09-27 15:44:54 -0400

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function submitData(name, email) {
     .then(function(response) {
         return response.json();
     })
-    .then (function(data) {
+    .then(function(data) {
         console.log(data)
     })
     .catch(function(error) {
